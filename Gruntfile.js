5de11d4919337f45e798c32c8cd9b8604b9bc6f8
hanjiang <670644959@qq.com>
2014-11-13 19:59:35 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,134 @@
+module.exports = function(grunt){
+ 
+    require('load-grunt-tasks')(grunt); //加载所有的任务
+ 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bower_concat: {
+          all: {
+            dest: 'lib/script/component.js',
+            cssDest: 'lib/style/component.css',
+            exclude: [
+             // 'jquery'
+            ],
+            dependencies: {
+                'cool-dialog': 'jquery'
+            }
+          }
+        },
+
+
+        connect: {
+            options: {
+                port: 9000,
+                hostname: '127.0.0.1', //默认就是这个值，可配置为本机某个 IP，localhost 或域名
+                livereload: 35729  //声明给 watch 监听的端口
+            },
+ 
+            server: {
+                options: {
+                    open: true, //自动打开网页 http://
+                    base: [
+                        'blog'  //主目录
+                    ]
+                }
+            }
+        },
+ 
+        watch: {
+            livereload: {
+                options: {
+                    livereload: '<%=connect.options.livereload%>'  //监听前面声明的端口  35729
+                },
+ 
+                files: [  //下面文件的改变就会实时刷新网页
+                    'blog/*.html',
+                    'blog/css/*.css',
+                    'blog/js/*.js',
+                    'blog/img/*.{png,jpg}'
+                ]
+            }
+        },
+
+        concat: {
+            options:{
+                separator: ';'
+            },
+
+            script: {
+                src: ['<%=bower_concat.all.dest%>','script/*.js'],
+                dest: 'blog/js/main.js'
+            },
+
+            style: {
+                src: ['<%=bower_concat.all.cssDest%>','style/*.css'],
+                dest: 'blog/css/style.css'
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'//添加banner
+            },
+
+            build: {
+                src: '<%=concat.script.dest%>',
+                dest: 'blog/js/main.min.js'
+            }
+        },
+
+        jshint: {
+            src: ['script/*.js']
+        },
+
+        handlebarslayouts: {
+          home: {
+            files: {
+              'blog/*.html': 'src/page/*.hbs'
+            },
+            options: {
+              partials: [
+                'src/template/*.hbs', 
+                'src/md/*.md',
+                'src/layout/*.html'
+              ],
+              modules: [
+                'src/helpers/helpers-*.js',
+                'handlebars-helper-moment',
+                //'handlebars-helper-feed'
+                //'handlebars-helper-md'
+              ],
+              basePath: 'src/',
+              context: {
+                title: 'Layout Test',
+                items: [
+                  'apple',
+                  'orange',
+                  'banana'
+                ]
+              }
+            }
+          }
+        }
+        
+    });
+ 
+    grunt.registerTask('serve', [
+        'connect:server',
+        'watch'
+    ]);
+
+    grunt.registerTask('compile', [
+        'bower_concat',
+        'concat',
+        'uglify'
+    ]);
+
+    grunt.registerTask('default', [
+        'concat',
+        'uglify',
+        'connect:server',
+        'watch'
+    ]);
+
+}
